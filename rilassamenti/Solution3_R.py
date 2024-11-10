b10d79b115fbeb8b9cae9c74b5698b9f44be75e2
silviaperelli <silviaperelli02@gmail.com>
2024-11-10 15:50:37 +0100

    Solution 3 e rilassamenti

---
+++ rilassamenti/Solution3_R.py
@@ -0,0 +1,64 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+n = 4 # Numero di job totali
+jobs_giocatore1 = [0, 1]  # Job assegnati al giocatore 1
+jobs_giocatore2 = [2, 3]  # Job assegnati al giocatore 2
+p = [3, 2, 4, 1]  # Durata di ogni job
+M = sum(p) # Big M (valore molto grande)
+alpha = 0.5 # Variabile alpha da far variare nella funzione obiettivo
+
+# Creazione del modello
+model = gp.Model("Solution3")
+
+# Variabili di inizio e completamento per ogni job
+s = model.addVars(n, vtype=GRB.CONTINUOUS, name="s")  # Tempo di inizio
+c = model.addVars(n, vtype=GRB.CONTINUOUS, name="c")  # Tempo di completamento
+
+# Creazione delle variabili di precedenza continue x[i,j] (x[i,j] ∈ [0, 1])
+x = model.addVars([(i, j) for i in range(n) for j in range(i+1, n)],vtype=GRB.CONTINUOUS,name='x')
+
+# Vincolo per limitare x tra 0 e 1
+for i in range(n):
+    for j in range(i+1, n):
+        model.addConstr(x[i, j] >= 0, f"x_lb_{i}_{j}")
+        model.addConstr(x[i, j] <= 1, f"x_ub_{i}_{j}")
+
+obj = (alpha * gp.quicksum(c[i] for i in jobs_giocatore1) + (1-alpha)*gp.quicksum(c[i] for i in jobs_giocatore2))
+
+# Funzione obiettivo: minimizzare la somma pesata dei tempi di completamento dei due giocatori
+model.setObjective(obj, GRB.MINIMIZE)
+
+# Vincoli sul tempo di completamento
+for i in range(n):
+    model.addConstr(c[i] == s[i] + p[i], f"completion_{i}")
+
+# Vincoli di precedenza
+for i in range(n):
+    for j in range(i+1, n):
+        # Caso 1: i precede j (x[i,j] = 1), allora s[j] >= c[i] - M * (1-x[i,j])
+        model.addConstr(s[j] >= c[i] - M * ( 1 - x[i, j]), f"precedenza_{i}_su_{j}")
+        # Caso 2: j precede i (x[i,j] = 0), allora s[i] >= c[j] - M * x[i,j]
+        model.addConstr(s[i] >= c[j] - M * x[i, j], f"precedenza_{j}_su_{i}")
+
+# Ottimizzazione del modello
+model.optimize()
+
+# Stampa dei risultati
+if model.status == GRB.OPTIMAL:
+    print("Soluzione ottima trovata:")
+    print(f"Valore di obj: {model.objVal}")
+    for i in range(n):
+        print(f"Job {i}: inizio = {s[i].x}, completamento = {c[i].x}")
+    for i in range(n):
+        for j in range(i+1, n):
+                print(f"x_{i}_{j} = {x[i, j].x}")
+
+    scheduling = sorted(range(n), key=lambda i: s[i].x)
+    print("\nScheduling trovato:")
+    for idx, job in enumerate(scheduling):
+        print(f"Posizione {idx+1}: Job {job}")
+else:
+    print("Non è stata trovata una soluzione ottima.")
+
+
